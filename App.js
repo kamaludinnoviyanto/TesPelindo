538e86f59a25a324815cdfe5f5cc613c781aa751
NOVIYANTOKAMALUDIN <noviyantokamaludin@gmail.com>
2024-01-04 17:05:31 +0700

    tes

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import Autocomplete from 'react-native-autocomplete-input';
 
-export default function App() {
-  return (
+const Form = () => {
+ const [negara, setNegara] = useState('');
+ const [pelabuhan, setPelabuhan] = useState('');
+ const [barang, setBarang] = useState('');
+ const [harga, setHarga] = useState('');
+ const [tarif, setTarif] = useState('');
+
+ const handleSubmit = () => {
+ };
+
+ const fetchData = () => {
+  fetch('https://insw-dev.ilcs.co.id/my/n/negara?ur_negara=SIN')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      setData(data);
+    })
+    .catch(error => {
+      console.error('Ada masalah dalam pengambilan data:', error);
+    });
+};
+
+useEffect(() => {
+  fetchData();
+}, []);
+
+ return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Negara</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setNegara(text)}
+        value={negara}
+        onPress={fetchData}
+      />
+
+      <Text>Pelabuhan</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setPelabuhan(text)}
+        value={pelabuhan}
+      />
+
+      <Text>Barang</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setBarang(text)}
+        value={barang}
+      />
+
+      <Text>Harga</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setHarga(text)}
+        value={harga}
+      />
+
+      <Text>Tarif Bea Masuk</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setTarif(text)}
+        value={tarif}
+      />
+
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
-  );
-}
+ );
+};
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingHorizontal: 10,
+ },
+ input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+ },
 });
+
+export default Form;
